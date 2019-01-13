f934a9f37ecd0cd72905dd0a8b673bdebc7a16fd
firdasafridi <firdasafridi@ymail.com>
2019-01-13 17:47:47 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+user$> brew cask install java
+
+
+user$> brew cask install homebrew/cask-versions/java8
+
+user$> brew services start zookeeper
+
+user$> brew install kafka
+
+
+user$> zkServer start
