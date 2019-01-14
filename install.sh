f17aaeb58526010f719eaf0b8acbb6f191d74dd2
firdasafridi <firdasafridi@ymail.com>
2019-01-14 10:54:34 +0700

    Update command

--- install.sh
+++ install.sh
@@ -9,3 +9,8 @@ user$> brew install kafka
 
 
 user$> zkServer start
+
+user$> brew services start kafka
+
+Or, if you don't want/need a background service you can just run:
+user$> zookeeper-server-start /usr/local/etc/kafka/zookeeper.properties & kafka-server-start /usr/local/etc/kafka/server.properties
